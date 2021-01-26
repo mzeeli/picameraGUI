dc0e2dea9cf56535e39e1df3557734aef43e9a06
Michael Li <michael.zeyuan@gmail.com>
2021-01-26 12:47:09 -0500

    Added magento optical trap object

---
+++ mot.py
@@ -0,0 +1,12 @@
+class MOT():
+    def __init__(self, img1, img2):
+        self.img1 = img1
+        self.img2 = img2
+
+    def getNumAtoms(self):
+        # Todo
+        return 'Todo'
+
+    def getMOTPosition(self):
+        # Todo
+        return [-22, 12, 2]  # [x, y, z] of the atom cloud
