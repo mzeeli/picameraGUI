5c220b8cb578b2c1187bc1d661ce9217bef8b043
Michael Li <michael.zeyuan@gmail.com>
2021-02-05 12:10:38 -0500

    Integrated second camera and added comments

--- main.py
+++ main.py
@@ -1,71 +1,73 @@
+from motCamera import MOTCamera
+from datetime import datetime
+from PIL import ImageTk, Image
+
 import tkinter as tk
 import tkinter.font as tkFont
 import numpy as np
 import threading
 import cv2
 
-from motCamera import MOTCamera
-from datetime import datetime
-from PIL import ImageTk, Image
-
-
-
-def resizeImage(imgPath, h, w):
-    img = Image.open(imgPath)
-    img = img.resize((w, h), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(img)
-    return img
-
-
 class PiCameraGUI(tk.Frame):
     def __init__(self, master, output=r"./saved_images", debug=False):
         self.master = master
         self.output = output
         self.debug = debug
-        self.width = 800  # Set main window width
+        self.mWidth = 800  # Set main window width
         self.mHeight = 600  # Set main window height
         self.defaultFont = 'Courier'  # Default font style
-        self.cam1 = MOTCamera()
+        self.cam0 = MOTCamera(1, grayscale=True)  # Pi compute module rease cam0 port as numerical value 1
+        self.cam1 = MOTCamera(0, grayscale=True)  # Pi compute module rease cam1 port as numerical value 0
+        
         tk.Frame.__init__(self, master)
-
-        self.mainDisplay = tk.Frame(master=self.master, height=self.mHeight, width=self.width,
+        
+        # Create main panel and show
+        self.mainDisplay = tk.Frame(master=self.master, height=self.mHeight, width=self.mWidth,
                                     highlightbackground="black", highlightthicknes=1)
         self.mainDisplay.pack()
 
+        # Default starting window
         self.showCameraWin()
 
+        # Add the navigation buttons at the bottom
         self.createNavigationBtn()
 
     def createNavigationBtn(self):
+        '''
+        Creates + displays the bottom navigation buttons and assigns 
+        their respective window display functions
+        '''
         btnHeight = 70
         btnWidth = 160
 
         btnFonts = tkFont.Font(family=self.defaultFont, size=15)
 
-        navigationFrame = tk.Frame(master=self.master, height=btnHeight+2, width=self.width,
+        navigationFrame = tk.Frame(master=self.master, height=btnHeight+2, width=self.mWidth,
                                    highlightbackground="black", highlightthicknes=1)
 
-        alignmentBtn = tk.Button(navigationFrame, text='Alignment', font=btnFonts,
-                                 command=self.showAlignmentWin)
+        # Create Buttons #
+        alignmentBtn = tk.Button(navigationFrame, text='Alignment', font=btnFonts, command=self.showAlignmentWin)
         alignmentBtn.place(x=0, y=0, height=btnHeight, width=btnWidth)
 
-        analysisBtn = tk.Button(navigationFrame, text='Analysis', font=btnFonts,
-                                command=self.showAnalysisWin)
+        analysisBtn = tk.Button(navigationFrame, text='Analysis', font=btnFonts, command=self.showAnalysisWin)
         analysisBtn.place(x=btnWidth, y=0, height=btnHeight, width=btnWidth)
 
-        cameraBtn = tk.Button(navigationFrame, text='Camera View', font=btnFonts,
-                              command=self.showCameraWin)
+        cameraBtn = tk.Button(navigationFrame, text='Camera View', font=btnFonts, command=self.showCameraWin)
         cameraBtn.place(x=btnWidth*2, y=0, height=btnHeight, width=btnWidth)
         
-        viewBtn = tk.Button(navigationFrame, text='3D View', font=btnFonts)
+        viewBtn = tk.Button(navigationFrame, text='3D View', font=btnFonts, command=self.show3DWin)
         viewBtn.place(x=btnWidth*3, y=0, height=btnHeight, width=btnWidth)
         
-        logBtn = tk.Button(navigationFrame, text='Log', font=btnFonts)
+        logBtn = tk.Button(navigationFrame, text='Log', font=btnFonts, command=self.showLogWin)
         logBtn.place(x=btnWidth*4, y=0, height=btnHeight, width=btnWidth)
 
         navigationFrame.pack()
 
     def showAlignmentWin(self):
+        '''
+        Creates widgets associated with the alignment window. 
+        This window is used to see how the MOT is aligned compared to the fiber
+        '''
         self.clearMainDisplay()
 
         # Create right side information panel #
@@ -117,8 +119,13 @@ class PiCameraGUI(tk.Frame):
                                   fill='slate gray')
 
     def showAnalysisWin(self):
+        '''
+        Creates widgets associated with the Analysis window. 
+        This window is used to calculate temperature and #Atoms
+        '''
         self.clearMainDisplay()
 
+        # Set fonts to be used in labels
         lblFont = tkFont.Font(family=self.defaultFont, size=25)
         dataFont = tkFont.Font(family=self.defaultFont, size=30)
 
@@ -128,100 +135,182 @@ class PiCameraGUI(tk.Frame):
 
         # Display temperature data
         tk.Label(self.mainDisplay, text=f'Temperature (mK)', font=lblFont)\
-            .place(x=self.width*3/10, y=self.mHeight/2-30, anchor='center')
+            .place(x=self.mWidth*3/10, y=self.mHeight/2-30, anchor='center')
         tk.Label(self.mainDisplay, text=f'{temp}', font=dataFont)\
-            .place(x=self.width*3/10, y=self.mHeight/2+30, anchor='center')
+            .place(x=self.mWidth*3/10, y=self.mHeight/2+30, anchor='center')
 
         # Display atom count data
         tk.Label(self.mainDisplay, text=f'#Atoms', font=lblFont)\
-            .place(x=self.width*7/10, y=self.mHeight/2-30, anchor='center')
+            .place(x=self.mWidth*7/10, y=self.mHeight/2-30, anchor='center')
         tk.Label(self.mainDisplay, text=f'{numAtoms}', font=dataFont)\
-            .place(x=self.width*7/10, y=self.mHeight/2+30, anchor='center')
+            .place(x=self.mWidth*7/10, y=self.mHeight/2+30, anchor='center')
 
     def showCameraWin(self):
+        '''
+        Creates widgets associated with the Camera window. 
+        This window is used to snap and save images from the camera and
+        create the video view popout window
+        ''' 
         self.clearMainDisplay()
+        
         # Main camera Displays #
         camDispHeight = 272;
         camDispWidth = 608;
 
-        #img1Path = r'./assets/img1.jpg'
-        img2Path = r'./assets/img2.jpg'
-        #img1 = resizeImage(img1Path, camDispHeight, camDispWidth)
-        img2 = resizeImage(img2Path, camDispHeight, camDispWidth)
+        cam0Lbl = tk.Label(self.mainDisplay, height=camDispHeight, width=camDispWidth, bd=1, relief='solid')
+        cam0Lbl.place(x=40, y=25)
+        threading.Thread(target=lambda : self.cam0.showImgOnLbl(cam0Lbl)).start() # Snap an image upon opening window
 
         cam1Lbl = tk.Label(self.mainDisplay, height=camDispHeight, width=camDispWidth, bd=1, relief='solid')
-        threading.Thread(target=lambda : self.cam1.showImgOnLbl(cam1Lbl)).start()
-        cam1Lbl.place(x=40, y=25)
-
-        cam2Lbl = tk.Label(self.mainDisplay, height=camDispHeight, width=camDispWidth, bd=1, relief='solid')
-        cam2Lbl.image = img2
-        cam2Lbl.configure(image=img2)
-        cam2Lbl.place(x=40, y=300)
+        cam1Lbl.place(x=40, y=300)
+        threading.Thread(target=lambda : self.cam1.showImgOnLbl(cam1Lbl)).start() # Snap an image upon opening window
 
         ## Camera Labels ##
         btnRelx = 0.91
-        
+        distY = 60
+        btnH = 60;
+        btnW = 65;
+
         camFont = tkFont.Font(family=self.defaultFont, size=13)
         tk.Label(self.mainDisplay, text='cam0', font=camFont, bg='gray83')\
             .place(x=41, y=26)
         tk.Label(self.mainDisplay, text='cam1', font=camFont, bg='gray83')\
             .place(x=41, y=301)
 
-        ## Video Button ##
-        vidImgPath = r'./assets/vid2.png'
-        vidImg = resizeImage(vidImgPath, 53, 57)
-        vidBtn = tk.Button(self.mainDisplay, relief=tk.GROOVE, text="Show Vid Stream",
-                           command=lambda : threading.Thread(target= lambda : self.cam1.showVid(608,464)).start())
+        ## Video Button for cam 0 ##
+        vidImgPath = r'./assets/vid_0.png'
+        vidImg = resizeImage(vidImgPath, btnH, btnW)
+        vidBtn = tk.Button(self.mainDisplay, relief=tk.GROOVE,
+                           command=lambda: threading.Thread(target=self.cam0.showVid).start() )
         vidBtn.image = vidImg
         vidBtn.configure(image=vidImg)
-        vidBtn.place(relx=btnRelx, y=120, anchor='center')
-        tk.Label(self.mainDisplay, text='Show Video')\
-            .place(relx=btnRelx, y=165, anchor='center')
+        vidBtn.place(relx=btnRelx, y=self.mHeight/2-3*distY, anchor='center')
+        # ~ tk.Label(self.mainDisplay, text='Show Vid 0').place(relx=btnRelx, y=105, anchor='center')
 
-        ## Camera Button ##
-        snapImgPath = r'./assets/snap2.png'
-        snapImg = resizeImage(snapImgPath, 53, 57)
-        snapBtn = tk.Button(self.mainDisplay, relief=tk.GROOVE,
-                        command=lambda : threading.Thread(target=lambda : self.cam1.showImgOnLbl(cam1Lbl)).start())
+        ## Snap Image Button ##
+        snapImgPath = r'./assets/snap.png'
+        snapImg = resizeImage(snapImgPath, btnH, btnW)
+        snapBtn = tk.Button(self.mainDisplay, relief=tk.GROOVE, 
+                            command=lambda : self.snapImages(cam0Lbl, cam1Lbl))
         snapBtn.image = snapImg
         snapBtn.configure(image=snapImg)
-        snapBtn.place(relx=btnRelx, y=285, anchor='center')
-        tk.Label(self.mainDisplay, text='Update Pictures')\
-            .place(relx=btnRelx, y=330, anchor='center')
+        snapBtn.place(relx=btnRelx, y=self.mHeight/2-distY, anchor='center')
+        # ~ tk.Label(self.mainDisplay, text='Snap Pictures').place(relx=btnRelx, y=259, anchor='center')
             
         ## Save Button ##
-        saveImgPath = r'./assets/save3.png'
-        saveImg = resizeImage(saveImgPath, 53, 57)
+        saveImgPath = r'./assets/save.png'
+        saveImg = resizeImage(saveImgPath, btnH, btnW)
         saveBtn = tk.Button(self.mainDisplay, relief=tk.GROOVE,
                             command=self.saveImage)
         saveBtn.image = saveImg
         saveBtn.configure(image=saveImg)
-        saveBtn.place(relx=btnRelx, y=450, anchor='center')
-        tk.Label(self.mainDisplay, text='Save Images')\
-            .place(relx=btnRelx, y=495, anchor='center')
+        saveBtn.place(relx=btnRelx, y=self.mHeight/2+distY, anchor='center')
+        # ~ tk.Label(self.mainDisplay, text='Save Images').place(relx=btnRelx, y=413, anchor='center')
+            
+            
+        ## Video Button for cam1 ##
+        vidImgPath = r'./assets/vid_1.png'
+        vidImg = resizeImage(vidImgPath, btnH, btnW)
+        vidBtn = tk.Button(self.mainDisplay, relief=tk.GROOVE,
+                           command=lambda: threading.Thread(target=self.cam1.showVid).start() )
+        vidBtn.image = vidImg
+        vidBtn.configure(image=vidImg)
+        vidBtn.place(relx=btnRelx, y=self.mHeight/2+3*distY, anchor='center')
+        # ~ tk.Label(self.mainDisplay, text='Show Vid 1').place(relx=btnRelx, y=567, anchor='center')
+        
+    def show3DWin(self):
+        '''
+        Creates widgets associated with the 3D window. 
+        This window is used to show a 3D view of the MOT based on two-view
+        geometry. Todo
+        ''' 
+        self.clearMainDisplay()
+        tk.Label(self.mainDisplay, text="Coming soon: matplotlib 3D view of structure")\
+                .place(relx=0.5, rely=0.5, anchor='center')
+                
+    def showLogWin(self):
+        '''
+        Creates widgets associated with the Log window. 
+        This window is used to show main commands used in the past and
+        system responses. Todo
+        ''' 
+        self.clearMainDisplay()
+        tk.Label(self.mainDisplay, text="Coming soon: log window")\
+                .place(relx=0.5, rely=0.5, anchor='center')
 
+
+    def snapImages(self, cam0Lbl, cam1Lbl):
+        '''
+        Snaps new images to the designated labels
+        cam0Lbl (tk.Label): Label to display image of cam0
+        cam1Lbl (tk.Label): Label to display image of cam1
+        '''
+        
+        threading.Thread(target=lambda : self.cam0.showImgOnLbl(cam0Lbl)).start()  
+        threading.Thread(target=lambda : self.cam1.showImgOnLbl(cam1Lbl)).start()  
 
     def saveImage(self):
+        '''
+        Saves the last images snapped by the cameras. Grab both images as
+        np.arrays and stack them veritcally. Finally saving them by their
+        timestamp in the output folder.
+        '''
+        # Create timestamp and file path
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        saveDir = f"{self.output}/{timestamp}.jpg"
-        cv2.imwrite(saveDir, self.cam1.img)
-        print(f"Image saved at {saveDir}")
-
+        savePath = f"{self.output}/{timestamp}.jpg"
+        
+        # Stack cam0 and cam1 images vertically
+        combinedImg = np.concatenate((self.cam1.img, self.cam0.img), axis=0)
+        
+        # Save combined image
+        cv2.imwrite(savePath, combinedImg)
+        
+        print(f"Image saved at {savePath}")
 
     def clearMainDisplay(self):
+        '''
+        Clears all widgets on the display to make room on the new window. 
+        Called at the start of every new window
+        '''
         for widget in self.mainDisplay.winfo_children():
             widget.destroy()
             
     def onWinClose(self):
+        '''
+        Callback for when the gui is closed, need to ensure the camera 
+        resources are released
+        '''
         print("Exiting Application")
+        self.cam0.close()
         self.cam1.close()
+
         self.master.destroy()
 
+def resizeImage(imgPath, h, w):
+    '''
+    Maps image to the dimensions of h and w and then returns it as an 
+    ImageTk.PhotoImage object to use in tkinter widgets
+    
+    Inputs
+    imgPath (str) = file path to image
+    h (int) = desired image height in pixels
+    w (int) = desired image width in pixels
+    
+    Output
+    img (ImageTk.PhotoImage) = image to use in tkinter widgets
+    '''
+    img = Image.open(imgPath)
+    img = img.resize((w, h), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    return img
 
 if __name__ == "__main__":
     window = tk.Tk()
     window.title('PiCamera')
+    
     gui = PiCameraGUI(window, debug=False)
+    
     window.protocol("WM_DELETE_WINDOW", gui.onWinClose)
     gui.pack()
+    
     window.mainloop()
--- motCamera.py
+++ motCamera.py
@@ -9,18 +9,35 @@ import tkinter as tk
 import numpy as np
 import threading
 
-
-
-
-
 class MOTCamera(picamera.PiCamera):
     
-    def __init__(self, grayscale=True):
-        picamera.PiCamera.__init__(self)
+    def __init__(self, camera_num, grayscale=True):
+        ''' 
+        camera_num [0,1]: determines which camera to open and reference
+            camera_num = 0: gets camera connected to CAM1 on COMPUTE MODULE
+            camera_num = 1: gets camera connected to CAM0 on COMPUTE MODULE
+        '''
+        
+        picamera.PiCamera.__init__(self, camera_num=camera_num)
+        
+        # Camera Properties #
+        # ~ self.sharpness = 0 # int:[-100, 100], default = 0
+        # ~ self.contrast = 0   # int:[-100, 100], default = 0
+        # ~ self.brightness = 50 # int:[0, 100], default = 0
+        # ~ self.saturation = 0   # int:[-100, 100], default = 0
+        # ~ self.iso = 0 # 100, 200, 320, 400, 500, 640, 800
+        # ~ self.exposure_mode = 'fireworks' # 'night', 'nightpreview', 'backlight', 'fireworks', default = 'auto'
+
+        # ~ self.shutter_speed  = 1800 # Shutter speed in microseconds, default = auto
+
+        
+        # Pi compute module reads cam0 port as numerical value 1 and vice versa, this converts 0->1 and 1->0
+        self.camNum = abs(camera_num - 1) 
         
         self.color_effects = (128,128) if grayscale else None  # By default set images to grayscale
         self.img = None  # field to store images
-        
+        self.windowName = f"Cam{self.camNum}, double left click to exit"
+        self.vidOn = False  # Tracks if video is recorded on a cv2 window
 
     def capImg(self, waitTime):
         self.resolution = (640, 480)
@@ -30,9 +47,12 @@ class MOTCamera(picamera.PiCamera):
         self.capture('foo.jpg')
     
     def showImgOnLbl(self, label):  
-        # Capture an image and display it on a tkinter label
-        # label: tkinter.Label
-        try:
+        '''
+        Capture an image and display it on a tkinter label
+        
+        label (tkinter.Label) = Target label to display snapped images
+        '''
+        try: # Low level error handling
             self.resolution = (608, 272)  # Default resolution to match GUI labels
             self.img = np.empty((272, 608, 3),dtype=np.uint8)
 
@@ -48,31 +68,54 @@ class MOTCamera(picamera.PiCamera):
 
 
     def showVid(self, rx=640, ry=480):
-        # Display continuous video stream on opencv window
-        # (rx, ry): resolution to display
+        '''
+        Display continuous video stream on separate opencv window, to 
+        exit double left click
         
-        print("Press 'q' to exit")
+        rx (int) = x resolution
+        ry (int) = y resolution
+        '''
+        print("Double left click to exit")
         self.resolution = (rx, ry)
         self.framerate = 80
-         
+        self.vidOn = True
         
         rawCapture = PiRGBArray(self)
         sleep(1)
         
+        
         for frame in self.capture_continuous(rawCapture, format="bgr", 
                                              use_video_port=True):
             image = frame.array
-        
-            cv2.imshow("Press 'q' to exit", image)
+            image = image[:,:,1]
+            cv2.imshow(self.windowName, image)
+            cv2.setMouseCallback(self.windowName, self.destroyWindows)
+            
+            
             key = cv2.waitKey(1)
             rawCapture.truncate(0)
             
-            if key == ord("q"):
+            if not self.vidOn:
+                cv2.waitKey(1)
                 cv2.destroyAllWindows()
+                print("Exiting video view")
                 break
+                
+                
+    def destroyWindows(self, event, x, y, flags, param):
+        '''
+        callback for mouse events, closes openCV window on double left click
+        '''
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            print("Double L click detected")
+            self.vidOn = False
                 
         
 if __name__ == "__main__":
-    camera = MOTCamera()
-    threading.Thread(target=camera.showVid).start()
+    cam1 = MOTCamera(0)
+    cam2 = MOTCamera(1)
+    
+    threading.Thread(target=cam1.showVid).start()
+
+
 
