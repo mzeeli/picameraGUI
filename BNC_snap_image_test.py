184f7112079df412d92c7b67770a11dbd04498db
Michael Li <michael.zeyuan@gmail.com>
2021-04-27 14:03:59 -0400

    modified BNC code to take a sequence of images at once

--- BNC_snap_image_test.py
+++ BNC_snap_image_test.py
@@ -9,43 +9,59 @@ import numpy as np
 Continuously check BNC connection and take picture when target GPIO 
 pin is set to high 640, 480
 """
+########################################
+# Params
+########################################
+# Number of images
+N = 10
+
 # Pin BNC is connected to
 pinBNC = 26
 
-# ~ img = cv2.imread("./saved_images/BNC timing tests/bnc_test.jpg")
-# ~ print(np.shape(img))
+roiX = 100
+roiY = 100
 
-saveImageName = "bnc_test.jpg"
-
+resX = 640
+resY = 480
+########################################
 # Setup cameras
+########################################
 camera = picamera.PiCamera()
 
-camera.color_effects = (128,128)
+camera.color_effects = (128, 128)
 camera.shutter_speed = 2000
-camera.resolution = (640,480)
+camera.resolution = (resX, resY)
 time.sleep(2)
-# ~ camera.capture("test.jpg")
-# ~ os.system("raspistill -t 0 -o os_test.jpg")
 
+imgs = np.empty((resY, resX, 3, N), dtype=np.uint8)
 
-# Listen for BNC signal
+########################################
+# Listen for BNC signal and save images
+########################################
 print("---Waiting for BNC---")
 try:
     GPIO.setmode(GPIO.BCM)  # USE GPIO# as reference 
     GPIO.setup(pinBNC, GPIO.IN)
-    
+
+    startTime = time.time()
     while True:
         # if pin is set to high take image
         if GPIO.input(pinBNC):
-            
-            # ~ os.syste("raspistill -o test.jpg")
-            
-            #Save newly acquired images 
-            camera.capture(f"./saved_images/BNC timing tests/{saveImageName}")
-            
-            print("Detected BNC signal and saved image")
-            break
-                            
+            print("Detected BNC signal")
+            for i in range(N):
+
+                # Capture newly acquired images
+                camera.capture(imgs[i])
+
+                break
+
+            timeTook = time.time() - startTime
+            print(f"Finished, took {timeTook} s")
 finally:
     # Upon exit reset ports to prevent damage
     GPIO.cleanup()
+
+########################################
+# Post image processing
+########################################
+
